1bfc76980f2d47a59956c126c40bcf96d93d8f41
Aliaume LOPEZ <ad.lopez@uw.edu.pl>
2024-05-23 17:55:16 +0200

    design choices

--- template/lipics-theorems.lua
+++ template/lipics-theorems.lua
@@ -43,3 +43,45 @@
 --
 -- TODO.
 
+-- First we list all the class names that we have to
+-- interact with:
+local classes = {
+  "theorem",
+  "lemma",
+  "conjecture",
+  "claim",
+  "corollary",
+  "proposition",
+  "definition",
+  "example",
+  "remark",
+  "proof"
+}
+
+-- For the HTML output, we create a theorem counter
+-- <div class="theorem" id="theorem-name">
+-- <h3><span>34</span>(theorem name).</h3>
+-- theorem content
+-- <details>
+-- <summary>Proof a</summary>
+-- </details>
+-- <details>
+-- <summary>Proof b</summary>
+-- </details>
+-- </div>
+
+-- initialise a theorem counter to 0
+local theorem_counter = 0
+
+-- Now we create a function that takes a Div element
+-- as input, and returns 
+local function Div(el)
+  -- check if the class of the Div element is in the list
+  if el.classes[1] in classes then
+    theorem_counter = theorem_counter + 1
+  end
+end
+
+{
+    { Div = Div }
+}
